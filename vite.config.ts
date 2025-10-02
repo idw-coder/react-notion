8e3bf5676bb4c724d14c1253beac6c5959582e53
ida-work <i21155388@gmail.com>
2025-10-02 18:37:53 +0900

    vite.config.tsにbaseパスを追加し、GitHub Pagesへのデプロイに対応。

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: '/react-notion/',  // 追加
   plugins: [react()],
   css: {
     postcss: {
