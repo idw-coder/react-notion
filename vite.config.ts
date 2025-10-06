2f226eda5c8bedd4be1d057a8f43a2a9018099fb
ida-work <i21155388@gmail.com>
2025-10-07 06:17:05 +0900

    vite.config.tsのbaseパスを環境変数VITE_BASE_PATHに変更し、deploy.ymlにVITE_BASE_PATHを設定。

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig(({ command }) => ({
-  base: command === 'build' ? '/react-notion/' : '/',
+  base: process.env.VITE_BASE_PATH || '/',
   plugins: [react()],
   css: {
     postcss: {
