ecfda6794b958af434d82369e1f9fd6ce3382f82
ida-work <i21155388@gmail.com>
2025-10-07 06:20:10 +0900

    bite.config.tsの修正

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import tailwindcss from 'tailwindcss';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig(({ command }) => ({
+export default defineConfig(({}) => ({
   base: process.env.VITE_BASE_PATH || '/',
   plugins: [react()],
   css: {
